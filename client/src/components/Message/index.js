d8b594adf113599e31ba73e357909c4c7f0fc8f7
S Mahidhar Rao <sandhinenimahidhar123@gmail.com>
2025-10-03 17:43:09 +0530

    Delete client/src/components/Message directory

--- client/src/components/Message/index.js
+++
@@ -1,9 +0,0 @@
-
-// Write your code here
-const Message = props => {
-  const {isLoggedin} = props
-  const message = isLoggedin ? 'Welcome User' : 'Please Login'
-  return <h1>{message}</h1>
-}
-
-export default Message
