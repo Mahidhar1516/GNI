d5c67568ef42e857191fdd46a85d0f71fa232388
S Mahidhar Rao <sandhinenimahidhar123@gmail.com>
2025-10-03 17:34:56 +0530

    Update index.js

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,1 +1,35 @@
 
+// Write your code here
+import './index.css'
+
+import {Component} from 'react'
+
+import Message from '../Message'
+import Login from '../Login'
+import Logout from '../Logout'
+
+class Home extends Component {
+  state = {isLoggedin: false}
+
+  LoginLogout = () => {
+    this.setState(prevState => ({isLoggedin: !prevState.isLoggedin}))
+  }
+
+  render() {
+    const {isLoggedin} = this.state
+
+    return (
+      <div>
+        <div>
+          <Message isLoggedin={isLoggedin} />
+          {isLoggedin ? (
+            <Logout logout={this.LoginLogout} />
+          ) : (
+            <Login login={this.LoginLogout} />
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+export default Home
