bbfa24f5a5a8d171885281a4310ec3c392469edc
S Mahidhar Rao <sandhinenimahidhar123@gmail.com>
2025-10-04 23:06:40 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 // src/App.js
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+import Register from './components/Register';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
