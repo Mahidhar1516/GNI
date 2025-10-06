ec33b462963a1a56b8278269708757202cfd4726
S Mahidhar Rao <sandhinenimahidhar123@gmail.com>
2025-10-06 13:08:30 +0530

    Create splashscreen.js

---
+++ src/Components/splashscreen.js
@@ -0,0 +1,18 @@
+// src/components/SplashScreen.jsx
+import React from "react";
+import "./SplashScreen.css";
+
+export default function SplashScreen() {
+  return (
+    <div className="splash-container">
+      <div className="splash-logo">
+        <img
+          src="/logo192.png"
+          alt="App Logo"
+          className="splash-image"
+        />
+        <h1 className="splash-title">Welcome to GNI</h1>
+      </div>
+    </div>
+  );
+}
